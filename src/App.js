22a37560f15a9d5c6a49363adc64140d308cce40
AntoineMeunier52 <meunier.antoine52@gmail.com>
2020-10-14 19:32:26 +0200

    add a array of users

--- src/App.js
+++ src/App.js
@@ -4,18 +4,8 @@ import Contact from './components/Contact'
 function App() {
   return (
     <div className="App">
-      <Contact imageSrc="https://randomuser.me/api/portraits/men/13.jpg" 
-               name="Lance Kim" 
-               statusText="Online" 
-               statusInd/>
-      <Contact imageSrc="https://randomuser.me/api/portraits/men/51.jpg" 
-               name="Armando Kelley" 
-               statusText="Offline"/>
-      <Contact imageSrc="https://randomuser.me/api/portraits/women/53.jpg" 
-               name="Sonia Taylor" 
-               statusText="Online"
-               statusInd/>
-    </div>
+      <Contact/>
+      </div>
   );
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,17 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Contact.css';
 
+const users = [
+    {
+      name: 'Robert Reyes',
+      avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
+      online: false
+    },
+    {
+      name: 'Nellie Caldwell',
+      avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
+      online: true
+    },
+    {
+      name: 'Vernon Mason',
+      avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
+      online: true
+    },
+    {
+      name: 'Erica Hunt',
+      avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
+      online: false
+    },
+    {
+      name: 'Juanita Phillips',
+      avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
+      online: true
+    }
+  ];
 
-const Contact = (props) => (
-    <div className="Contact">
-            <img className="avatar" src={props.imageSrc}/>
-        <div> 
-            <p className="name">{props.name}</p>
-            <div className="status">
-            <div className={props.statusInd ? 'status-online' : 'status-offline'}></div>
-                <p className="status-text">{props.statusText}</p>
-            </div>
-        </div>
+const Contact = () => (
+    <div>
+        {users.map(item => (
+            <div className="Contact"> 
+                <img className="avatar" src={item.avatar}/>
+                <div> 
+                    <p className="name">{item.name}</p>
+                    <div className="status">
+                        <div className={item.statusInd ? 'status-online' : 'status-offline'}></div>
+                        <p className="status-text">{item.statusText}</p>
+                    </div>
+                </div>
+            </div>  
+        ))}
     </div>
 );
 
